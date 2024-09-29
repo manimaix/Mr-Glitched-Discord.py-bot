41b9774a5cd00886fe40e8282720bbc6bb2b6b01
manimaix <138971195+manimaix@users.noreply.github.com>
2024-09-29 17:41:45 +0800

    Mr_Glitched.py

--- Mr_Glitched.py
+++ Mr_Glitched.py
@@ -19,7 +19,7 @@ send_pings_enabled = True
 
 # Cooldown dictionary to track the last time the bot was mentioned
 cooldown = {}
-allowed_channel_ids = [ 1240805113136681051]
+allowed_channel_ids = [1240805113136681051]
 category_id = 1240844403082268712  
 
 @bot.event
@@ -57,6 +57,12 @@ async def send_pings(channel):
         send_pings_enabled = True  # Reset send_pings_enabled to True to resume pings
         return  # Exit the function if send_pings_enabled is False
 
+async def send_marijester_links(channellink, messagelink, detected):
+    channel = bot.get_channel(channellink)
+    if channel:
+        await channel.send(f"{detected} is Found!\n Detected in: {messagelink}")
+
+
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
@@ -78,7 +84,21 @@ async def on_message(message):
         await message.channel.send("Mass Pinging have stopped.")
         await asyncio.sleep(1)
         send_pings_enabled = True
+    if message.author.bot and any(word in message.content.lower() for word in ['mari', 'jester']) and message.channel.category_id == category_id:
+        message_link = f"https://discord.com/channels/{message.guild.id}/{message.channel.id}/{message.id}"
+        detected = []
 
+        # Check for "Mari" and "Jester" in the message content
+        if 'mari' in message.content.lower():
+            detected.append('<@&1255540456352256010> ***Mari***')
+        if 'jester' in message.content.lower():
+            detected.append('<@&1255540361229631580> ***Jester***')
+
+        # Join the detected names with " and " if both are present
+        detected_str = " and ".join(detected)
+        
+        # Send the detected message link with the detected names
+        await send_marijester_links(1288275351155249217, message_link, detected_str)
 
 
 # Run the bot
